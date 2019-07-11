66464008356c19b3a3bb2b4f4c53296e45e618fc
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-07-10 20:55:59 -0700

    Initial file setup and database creation.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('A1 Peppercorn Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Full Stack Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mozzarella Brioche Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Good Morning Burger', false);
